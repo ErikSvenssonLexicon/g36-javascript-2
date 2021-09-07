03b70e85bf8a89472984f45fc37ff46487802855
Erik Svensson <eosvensson@gmail.com>
2021-09-07 14:23:23 +0200

    Added remove

--- src/lecture.js
+++ src/lecture.js
@@ -1,93 +1,103 @@
-
 const state = {
-    people: [],
-    formValues: {
-        inputNameValue: "",
-        inputBirthDateValue: ""
-    }    
-}
+  people: [],
+  formValues: {
+    inputNameValue: "",
+    inputBirthDateValue: "",
+  },
+};
 
-const createPerson = ({inputNameValue, inputBirthDateValue}) => {
-    return {
-        name: inputNameValue,
-        birthDate: new Date(inputBirthDateValue),
-        getAge(){
-            return new Date().getUTCFullYear() - this.birthDate.getUTCFullYear();
-        }
-    }
-}
+const createPerson = ({ inputNameValue, inputBirthDateValue }) => {
+  return {
+    id: `${Math.floor(Math.random() * 1000000)}`,
+    name: inputNameValue,
+    birthDate: inputBirthDateValue,
+    getAge() {
+      return (
+        new Date().getUTCFullYear() - new Date(this.birthDate).getUTCFullYear()
+      );
+    },
+  };
+};
 
-const handleFormSubmit = event => {
-    event.preventDefault();
-    const person = createPerson(state.formValues);
-    state.people = [...state.people, person];
-    document.getElementById("name").value = "";
-    document.getElementById("birthDate").value = "";
-    renderPeople(state);    
-}
+const handleFormSubmit = (event) => {
+  //Preventing default submit event from refreshing the page
+  event.preventDefault();
 
-const handleNameChange = event => state.formValues.inputNameValue = event.target.value;
-const handleBirthDateChange = event => state.formValues.inputBirthDateValue = event.target.value;
+  const person = createPerson(state.formValues);
+  state.people = [...state.people, person];
+
+  //Cleaning up
+  document.getElementById("name").value = "";
+  document.getElementById("birthDate").value = "";
+  state.formValues.inputBirthDateValue = "";
+  state.formValues.inputNameValue = "";
+
+  //Calling render after adding to array
+  renderPeople(state);
+};
+
+const handleNameChange = (event) =>
+  (state.formValues.inputNameValue = event.target.value);
+const handleBirthDateChange = (event) =>
+  (state.formValues.inputBirthDateValue = event.target.value);
+
+const handleRemove = element => {
+    state.people = state.people.filter(person => person.id !== element.id);
+    renderPeople(state);
+}
 
 document.getElementById("myForm").addEventListener("submit", handleFormSubmit);
 document.getElementById("name").addEventListener("change", handleNameChange);
 document.getElementById("birthDate").addEventListener("change", handleBirthDateChange);
 
+const createElement = (type, ...classes) => {
+  const element = document.createElement(type);
+  if (classes.length > 0) {
+      for(css of classes){
+          element.classList.add(css);
+      } 
+  }
 
-const createElement = (type, ...classes) =>{
-    const element = document.createElement(type);
-    if(classes.length > 0){
-        element.classList.add(classes.join(' '));
-    }
+  return element;
+};
+
+const renderPeople = ({ people }) => {
+  const container = document.getElementById("people");
+  container.innerHTML = "";
+
+  if (people.length > 0) {
+    const table = createElement("table", "table", "table-hover");
+    const thead = createElement("thead");
     
-    return element;
-}
+    const tHeadRow = 
+        `<tr>
+            <th scope="col">Name</th> 
+            <th scope="col">Birth Date</th> 
+            <th scope="col">Age</th> 
+        </tr>`;
 
-const renderPeople = ({people}) =>{
-    const container = document.getElementById("people");
-    container.innerHTML = "";
-    
-    
-    if(people.length > 0){
-        const table = createElement("table", "table");        
-        const thead = createElement("thead");        
-        const tr = createElement("tr")
-        const th = document.createElement("th");
-        th.scope = "col";
-        th.innerHTML = "Name";
+    thead.innerHTML = tHeadRow;
 
-        const th2 = document.createElement("th");
-        th2.scope = "col";
-        th2.innerHTML = "Birth Date";
+    const tbody = document.createElement("tbody");
+    const trs = people.map((person) => {
 
-        const th3 = document.createElement("th");
-        th3.scope = "col";
-        th3.innerHTML = "Age";        
-        tr.appendChild(th);
-        tr.appendChild(th2);
-        tr.appendChild(th3);        
-        thead.appendChild(tr);
-        
-        
-        const tbody = document.createElement("tbody");
-        const trs = people.map(person => {
-            const row = `<tr><td>${person.name}</td><td>${person.birthDate}</td><td>${person.getAge()}</td></tr>`
-            return row;
-        })
-        tbody.innerHTML = trs.join('');
+      const row = `<tr onclick="handleRemove(this)" id=${person.id}>
+        <td>${person.name}</td>
+        <td>${person.birthDate}</td>
+        <td>${person.getAge()}</td>
+      </tr>`;
+      return row;
+    });
+    tbody.innerHTML = trs.join("");
 
-        table.appendChild(thead);
-        table.appendChild(tbody); 
-        
-        container.appendChild(table);
+    table.appendChild(thead);
+    table.appendChild(tbody);
 
-    }else{
-        const paragraph = `<p class="text-center">Please add some people</p>`;
-        container.innerHTML = paragraph;
-    }
-
-
-}
+    container.appendChild(table);
+  } else {
+    const paragraph = `<p class="text-center">Please add some people</p>`;
+    container.innerHTML = paragraph;
+  }
+};
 
 renderPeople(state);
-
